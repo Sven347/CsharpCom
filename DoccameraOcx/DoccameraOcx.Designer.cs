c2e3872ac4dcec37bb035eec2d44ea1eac90cae1
shandinan <shandinan@live.com>
2018-09-18 14:44:44 +0800

    sdn

--- DoccameraOcx/DoccameraOcx.Designer.cs
+++ DoccameraOcx/DoccameraOcx.Designer.cs
@@ -29,29 +29,19 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DoccameraOcx));
-            this.sdnZGOcx = new AxCmCaptureOcxLib.AxCmCaptureOcx();
             this.tabMain = new System.Windows.Forms.TabControl();
             this.tabOne = new System.Windows.Forms.TabPage();
             this.tabTwo = new System.Windows.Forms.TabPage();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.sdnZGOcx)).BeginInit();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.sdnZGOcx = new AxCmCaptureOcxLib.AxCmCaptureOcx();
             this.tabMain.SuspendLayout();
             this.tabOne.SuspendLayout();
             this.tabTwo.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sdnZGOcx)).BeginInit();
             this.SuspendLayout();
-            // 
-            // sdnZGOcx
-            // 
-            this.sdnZGOcx.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.sdnZGOcx.Enabled = true;
-            this.sdnZGOcx.Location = new System.Drawing.Point(3, 3);
-            this.sdnZGOcx.Name = "sdnZGOcx";
-            this.sdnZGOcx.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("sdnZGOcx.OcxState")));
-            this.sdnZGOcx.Size = new System.Drawing.Size(980, 580);
-            this.sdnZGOcx.TabIndex = 0;
             // 
             // tabMain
             // 
@@ -67,20 +57,20 @@
             // tabOne
             // 
             this.tabOne.Controls.Add(this.sdnZGOcx);
-            this.tabOne.Location = new System.Drawing.Point(4, 28);
+            this.tabOne.Location = new System.Drawing.Point(4, 22);
             this.tabOne.Name = "tabOne";
             this.tabOne.Padding = new System.Windows.Forms.Padding(3);
-            this.tabOne.Size = new System.Drawing.Size(986, 586);
+            this.tabOne.Size = new System.Drawing.Size(986, 592);
             this.tabOne.TabIndex = 0;
             this.tabOne.Text = "高拍仪";
             // 
             // tabTwo
             // 
             this.tabTwo.Controls.Add(this.panel1);
-            this.tabTwo.Location = new System.Drawing.Point(4, 28);
+            this.tabTwo.Location = new System.Drawing.Point(4, 22);
             this.tabTwo.Name = "tabTwo";
             this.tabTwo.Padding = new System.Windows.Forms.Padding(3);
-            this.tabTwo.Size = new System.Drawing.Size(986, 586);
+            this.tabTwo.Size = new System.Drawing.Size(986, 592);
             this.tabTwo.TabIndex = 1;
             this.tabTwo.Text = "活体检测";
             this.tabTwo.UseVisualStyleBackColor = true;
@@ -92,24 +82,34 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(980, 580);
+            this.panel1.Size = new System.Drawing.Size(980, 586);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 514);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(980, 66);
-            this.panel2.TabIndex = 0;
             // 
             // panel3
             // 
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(980, 514);
+            this.panel3.Size = new System.Drawing.Size(980, 520);
             this.panel3.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 520);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(980, 66);
+            this.panel2.TabIndex = 0;
+            // 
+            // sdnZGOcx
+            // 
+            this.sdnZGOcx.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sdnZGOcx.Enabled = true;
+            this.sdnZGOcx.Location = new System.Drawing.Point(3, 3);
+            this.sdnZGOcx.Name = "sdnZGOcx";
+            this.sdnZGOcx.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("sdnZGOcx.OcxState")));
+            this.sdnZGOcx.Size = new System.Drawing.Size(980, 586);
+            this.sdnZGOcx.TabIndex = 0;
             // 
             // DoccameraOcx
             // 
@@ -117,11 +117,11 @@
             this.Name = "DoccameraOcx";
             this.Size = new System.Drawing.Size(994, 618);
             this.Load += new System.EventHandler(this.DoccameraOcx_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.sdnZGOcx)).EndInit();
             this.tabMain.ResumeLayout(false);
             this.tabOne.ResumeLayout(false);
             this.tabTwo.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sdnZGOcx)).EndInit();
             this.ResumeLayout(false);
 
         }
